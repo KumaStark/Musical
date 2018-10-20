a389000db3d03a6de5697048c1ad17148a03051b
KumaStark <elastic48@msn.com>
2018-10-20 22:26:56 +0800

    Initial commit

---
+++ app/src/main/java/kuma/project/udacity/musical/MainActivity.java
@@ -0,0 +1,13 @@
+package kuma.project.udacity.musical;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
